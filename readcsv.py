461e6fbefbcfc8776ab018241c29ddfb11bafced
Naiden Boro <n.t.borodjiev@gmail.com>
2023-01-15 12:20:22 +0200

    Initial commit

---
+++ readcsv.py
@@ -0,0 +1,56 @@
+import csv
+
+kv = []
+stai = []
+info = []
+with open('data.csv', newline='',encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        info=info+row[4].split('+')
+        kv.append(row[0])
+        stai.append(row[2])
+
+
+kv = list(set(kv))
+stai = set(stai)
+stai.remove('Гараж')
+stai = list(stai)
+info =list(set(info))
+print(kv)
+print(stai)
+print(info)
+
+
+def one_hot(el, lst):
+    result = [0] * len(lst)
+    result[lst.index(el)] = 1
+    return result
+
+def tokenize(elements, lst):
+    result = [0] * len(lst)
+    for el in elements:
+        result[lst.index(el)] = 1
+    return result
+
+spr=','
+my_home = 'Център,София,Тристаен,80,Обзаведен+Тухла+ТЕЦ,161990'.split(',')
+output = spr.join(str(x) for x in one_hot(my_home[0],kv))+spr
+output += spr.join(str(x) for x in one_hot(my_home[2],stai))+spr
+output += my_home[3] + spr
+output += spr.join(str(x) for x in tokenize(my_home[4].split('+'),info))+spr
+output += my_home[5]
+print (output)
+
+#
+# f = open("demofile3.csv", "a",encoding="utf-8")
+# with open('data.csv', newline='',encoding='utf-8') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#     for row in spamreader:
+#         if row[2] != 'Гараж':
+#             output = spr.join(str(x) for x in one_hot(row[0],kv))+spr
+#             output += spr.join(str(x) for x in one_hot(row[2],stai))+spr
+#             output += row[3] + spr
+#             output += spr.join(str(x) for x in tokenize(row[4].split('+'),info))+spr
+#             output += row[5]
+#             f.write(output+"\n")
+# f.close()
---
+++ scrapper.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+#x = requests.get('https://www.homes.bg/api/offers?startIndex=0&stopIndex=0')
+
+#json.loads(x.text)['result']
+appartaments = []
+for i in range (140):
+    x = requests.get('https://www.homes.bg/api/offers?startIndex=%s&stopIndex=%d' % (i, i+99))
+    appartaments = appartaments+json.loads(x.text)['result']
+    print(i)
+
+f = open("demofile2.txt", "a",encoding="utf-8")
+for ap in appartaments:
+    
+    if(ap['price']['currency'] != 'EUR'):
+        continue
+    str = ap['location'] + ',' + ap['title'] + ',' + ap['description'].replace(',','+')+',' + ap['price']['value'].replace(',','')+'\n'
+    str=str.replace(' ','')
+    str=str.replace('m²','')
+    f.write(str)
+f.close()
